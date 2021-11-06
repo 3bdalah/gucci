59ec1e21b2762e9d2133e20d956a95a31c238431
3bdalah <3bdallah.dev@gmail.com>
2021-11-06 04:05:07 +0200

    feat: create page about -feat hero component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+         Labore pariatur consequat anim labore incididunt do ea non dolor ad adipisicing anim. Elit in cupidatat minim duis reprehenderit veniam ea tempor aute nulla ut ad non cillum. Reprehenderit mollit excepteur eu exercitation veniam nisi dolor nisi est nulla nulla irure. Velit eiusmod duis enim culpa anim laboris enim ex.
+         Fugiat commodo fugiat veniam nostrud sint esse. Sit id aute qui qui do exercitation. Consequat magna eiusmod adipisicing incididunt ut velit amet est aute incididunt duis magna. Fugiat veniam amet voluptate cillum ut sint.
+         Cillum esse et amet consequat dolor eiusmod aliquip et anim est deserunt consectetur. Ad aute eu sit magna consectetur ut enim adipisicing labore sit mollit mollit cillum. Labore anim enim aliquip est ex ex velit ut commodo consectetur dolor. Cillum sunt Lorem culpa et irure veniam et exercitation tempor. Do dolore ipsum incididunt in do fugiat ullamco nisi ex.
+      </p>
+    </section>
+  )
 }
