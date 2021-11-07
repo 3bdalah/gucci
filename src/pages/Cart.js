eff557ee773e18df794f483d3cfcae7aabb37053
3bdalah <3bdallah.dev@gmail.com>
2021-11-07 02:48:36 +0200

    feat cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,12 +5,21 @@ import CartItem from "../components/Cart/EmptyCart";
 import {Link} from "react-router-dom";
 
 export default function Cart() {
-  let user = false;
+  let user = true;
   const {cart,total} = useContext(CartContext);
   console.log({cart,total});
 
    if(cart.length === 0){ 
       return <EmptyCart/>
    }
-  // return <h1>hello from cart page</h1>;
+   return (
+     <secction className="cart-items section">
+      <h2>your cart</h2>
+      {cart.map(item => {
+        return <CartItem key={item.id} {...item}/>
+      })}
+      <h2>total : ${total}</h2>
+      {user ? (<Link to="/checkout" className="btn btn-primary btn-block">Checkout</Link> ): (<Link to="/login" className="btn btn-primary btn-block">Login</Link>)}
+     </secction>
+   )
 }
