92c105ab19ca8889a011da23d3d52183b3d88210
3bdalah <3bdallah.dev@gmail.com>
2021-11-07 04:30:54 +0200

    feat calculate cart total and cart items

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,13 +1,46 @@
 // cart context
-import React,{createContext, useState} from "react";
+import React,{createContext, useState, useEffect} from "react";
 import localCart from "../utils/localCart";
 const CartContext = createContext();
  function CartProvider({children}){
-     const [cart, setCart] = useState([]);
+     const [cart, setCart] = useState(localCart);
      const [total,setTotal] = useState(0);
      const [cartItems,setCartItems] = useState(0);
+
+   
+     useEffect(()=>{
+         let newCartItems = cart.reduce((total,cartItem)=>{
+             console.log({total,cartItem});
+             return (total += cartItem.amount);
+         },0);
+        setCartItems(newCartItems);
+        let newTotal = cart.reduce((total,cartItem) => {
+            return total += (cartItem.amount*cartItem.price);
+        },0);
+        newTotal = parseFloat(newTotal.toFixed(2));
+        setTotal(newTotal);
+     },[cart]);
+
+
+     // remove item
+     const removeItem = id => {
+
+     }
+    //  increacs amount 
+    const increaseAmount = id => {};
+    
+    // decrease amount 
+    const decreaseAmount = id => {};
+
+    // add to cart 
+    const addToCart = id => {};
+
+    // clear Cart 
+    const clearCart = id => {};
+
+
     return <CartContext.Provider value={{
-        cart,total,cartItems
+        cart,total,cartItems,increaseAmount,removeItem,decreaseAmount,addToCart,clearCart
     }}>
     {children}
     </CartContext.Provider>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,25 +1,31 @@
 import React,{useContext} from "react";
 import {CartContext} from "../context/cart";
 import EmptyCart from  "../components/Cart/EmptyCart";
-import CartItem from "../components/Cart/EmptyCart";
+import CartItem from "../components/Cart/CartItem";
 import {Link} from "react-router-dom";
 
 export default function Cart() {
   let user = true;
-  const {cart,total} = useContext(CartContext);
-  console.log({cart,total});
-
+   const {cart,total} = useContext(CartContext);
+   console.log('cart' , cart);
    if(cart.length === 0){ 
       return <EmptyCart/>
-   }
-   return (
-     <secction className="cart-items section">
-      <h2>your cart</h2>
-      {cart.map(item => {
-        return <CartItem key={item.id} {...item}/>
-      })}
-      <h2>total : ${total}</h2>
-      {user ? (<Link to="/checkout" className="btn btn-primary btn-block">Checkout</Link> ): (<Link to="/login" className="btn btn-primary btn-block">Login</Link>)}
-     </secction>
-   )
+   }else{
+     return ( <section className="cart-items section">
+     <h2>your cart</h2>
+     {cart.map(item => {
+       return <CartItem key={item.id} {...item} />;
+     })}
+     <h2>total : $ {total}</h2>
+     {user? (
+       <Link to="/checkout" className="btn btn-primary btn-block">
+         checkout
+       </Link>
+     ) : (
+       <Link to="/login" className="btn btn-primary btn-block">
+         login
+       </Link>
+     )}
+ </section>);
+  }
 }
