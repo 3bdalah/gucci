35a5fffc8640d5980eb3a09b4c2d2402378bf11d
3bdalah <3bdallah.dev@gmail.com>
2021-11-13 11:08:49 +0200

    fix login setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,16 +9,43 @@ export default function Login() {
   // state value  
   let [email,setEmail] = useState("");
   let [password, setPassword] = useState("");
-  let [usename,setUserName] = useState("default");
-  
-  let [isMember,setIsMember] = useState(true);
-  let isEmpty = false;
+  let [username,setUsername] = useState("");
+  let [isMember,setIsMember] = useState(false);
+  let isEmpty = true;
   const toggleMember = () => {
 
   }
   const handleSubmit = async (e) => {
 
   }
-  return <h1>hello from login page</h1>;
+  return (
+    <section className="form section">
+     <h2 className="section-title">{isMember? "sign in " : "register"}</h2>
+     <form className="login-form">{}
+     <div className="form-control">
+       <label htmlFor="email">email</label>
+       <input type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+     </div>
+     <div className="form-control">
+       <label htmlFor="password">password</label>
+       <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+     </div>
+      {!isMember && (
+        <div className="form-control">
+          <label htmlFor="username">username</label>
+
+          <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)}/>
+        </div>
+      )};
+      {isEmpty  && (<p className="form-empty">please fill out all form fields </p>)}
+      {!isEmpty && (<button type="submit" className="btn btn-primary btn-block" onClick={handleSubmit}>Submit</button>)}
+     
+     <p className="register-link">
+       {isMember ? "need  to register": "already a member"}
+       <button  type="button" onClick={toggleMember}>click here</button>
+     </p>
+     </form>
+    </section>
+  );
  
 }
